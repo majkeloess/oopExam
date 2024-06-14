59e13b7d0d4f50939c62479322d5e924d7dde7c1
majkeloess <msaturczak@student.agh.edu.pl>
2024-06-14 23:25:09 +0200

    Wiecej dymu

--- pomocowe/egz7.cpp
+++ pomocowe/egz7.cpp
@@ -1,19 +1,65 @@
+#include <iostream>
+
+template <typename T>
+class B;
+
+template <typename T>
+class B
+{
+private:
+    T m_val{};
+
+public:
+    using value_type = T;
+
+    explicit B(T val) : m_val{val} {}
+
+    T get() const
+    {
+        return m_val;
+    }
+};
+
+// Ta część definiuje specjalizację szablonu klasy B dla przypadku, gdy parametr T jest wskaźnikiem(T *).Oznacza to, że ta klasa będzie działać inaczej niż ogólna klasa B<T> dla typów innych niż wskaźniki.
+
+// Celem tej specjalizacji jest zapewnienie, że obiekty B<T *> będą zawsze przechowywać unikalne wskaźniki i nie będzie można ich kopiować ani przypisywać do innych obiektów.Jest to typowe podejście, gdy chcemy kontrolować czas życia obiektu wskazywanego przez wskaźnik.W tym przypadku, odpowiedzialność za zwolnienie pamięci należy do właściciela obiektu B<T *>
+
+template <typename T>
+class B<T *>
+{
+public:
+    T *m_val{};
+
+public:
+    using value_type = T;
+
+    B(T *val) : m_val{val} {}
+    B(const B<T *> &) = delete;
+    B<T *> &operator=(const B<T *> &) = delete;
+    T get() const
+    {
+        return *m_val;
+    }
+};
+
 int main()
 {
     typedef B<int> int_b;
-    typedef B<int*> int_p_b;        //jednak typedef
+    typedef B<int *> int_p_b; // jednak typedef
 
     int_b a(1);
     int_b a_copy(a);
     int_p_b b(new int_p_b::value_type{2});
-    // int_p_b e = b; //error
-    // b=b //error
-    
-    std::cout << "a=" << a.get() << std::endl;
-    std::cout << "a_copy=" << a_copy.get()+1 << std::endl;
-    std::cout <<" b=" << b.get()+1 << std::endl;
+    // int_p_b e = b; // error
+    // b = b;         // error
+
+    std::cout
+        << "a=" << a.get() << std::endl;
+    std::cout << "a_copy=" << a_copy.get() + 1 << std::endl;
+    std::cout << " b=" << b.get() + 1 << std::endl;
 }
 /*
 a=1
 a_copy=2
- b=3
+b=3
+*/
--- pomocowe/egz9.cpp
+++ pomocowe/egz9.cpp
@@ -1,13 +1,20 @@
+#include <iostream>
+#include <utility>
+
 int main()
 {
-    cout << "a->"; A a;
-    cout << "b->"; A b(1);
-    cout << "c->"; A c(1,2);
+    cout << "a->";
+    A a;
+    cout << "b->";
+    A b(1);
+    cout << "c->";
+    A c(1, 2);
 
-    cout << "d->"; A d = std::move(c);
+    cout << "d->";
+    A d = std::move(c);
 
-    //A e = c;
-    //A f = 1;
+    // A e = c;
+    // A f = 1;
 }
 /*
 a-> A::A(int,int): _a=0,_b=0
